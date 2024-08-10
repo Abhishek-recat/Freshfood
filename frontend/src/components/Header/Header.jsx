11e0f45bbeff2140ccf98f6b8a131ae8518d3f68
Abhishek-recat <abhigowda886269@gmail.com>
2024-08-10 09:10:53 -0700

    firstcomit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <Div className="header-content">
+            <h2>Order your Favourite Food Here</h2>
+            <p>choose from a diverse menu featuring a delecatble array of dishes crafted with the finest ingredients and culinary expertise. our satisfying your cravings  and elevate your dining  expirience,one delicious meal at a time .</p>
+            <button>View Menu</button>
+        </Div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Placeorder/Placeorder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Placeorder from './Placeorder.css'
+
+const Placeorder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Placeorder
